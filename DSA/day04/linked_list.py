0c229c59f6a2d88793c733a812230fe6ed063017
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-04-16 06:45:40 +0530

    the basic strcture of linked list was done, now going to implement that inside imlementation

---
+++ DSA/day04/linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beggining(self, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def insert_at_index(self, ind, data):
+        pass
+
+    def list_size(slelf):
+        pass
+
+    def insert_after(self,ind, data):
+        pass
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    
