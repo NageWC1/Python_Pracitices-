2d538e4f2683e7c3ec5386b90bb8b550810d590f
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-04-16 07:20:02 +0530

    basics for stack was done

---
+++ DSA/day04/stack.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.stack = deque()
+
+    def insert(self, data):
+        pass
+
+    def delete(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
+if __name__ == "__main__":
+    s = Stack()
+    s.insert(10)
+    s.insert(11)
+    s.insert(12)
+    s.insert(13)
+    
