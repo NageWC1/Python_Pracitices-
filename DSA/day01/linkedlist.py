350067174cfb478e409808ffd21bdba87ae6f2ea
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-02 11:56:37 +0530

    the linked list basic functions were done

--- DSA/day01/linkedlist.py
+++ DSA/day01/linkedlist.py
@@ -5,14 +5,40 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        head = None
+        self.head = None
     
     def insert_at_beggining(self,data):
-        pass
+        if self.head is None:
+            node = Node(data,None)
+            self.head = node
+
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self,data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+        
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data,None) 
+
+    def print(self):
+        l_str = ""
+        itr = self.head
+
+        while itr:
+            l_str += str(itr.data) + "-->s"
+            itr = itr.next
+        print(l_str)
 
 
 if __name__ == "__main__":
-    pass
+    ll = LinkedList()
+
+    ll.insert_at_beggining(10)
+    ll.insert_at_beggining(11)
+    ll.insert_at_beggining(12)
+    ll.print()
