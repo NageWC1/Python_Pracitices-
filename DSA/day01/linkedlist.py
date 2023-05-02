3ed9a4e5de77de9c7a6ffa1efd58d1faf7fa223b
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-02 11:46:07 +0530

    doen the basic for linked list

---
+++ DSA/day01/linkedlist.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,data:None,next:None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        head = None
+    
+    def insert_at_beggining(self,data):
+        pass
+
+    def insert_at_end(self,data):
+        pass
+
+
+if __name__ == "__main__":
+    pass
