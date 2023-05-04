f880ed85c97862a73b3e0e04f402b8810f68479c
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-04 10:24:47 +0530

    added the basic file and added the basic functions for linked list - next will be the implmentation of function

---
+++ DSA/day03/linkedlist.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beggining(self, data):
+        pass 
+
+    def insert_at_end(self, data):
+        pass
+
+    def remove_at(self, ind):
+        pass
+
+if __name__ == "__main__":
+    l = LinkedList()
