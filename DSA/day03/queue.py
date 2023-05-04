05a669531f4b442243c742fc0c1a06be97fb4154
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-04 10:58:38 +0530

    the basic functionaly of queue was done

---
+++ DSA/day03/queue.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def enqueue(self, data):
+        self.buffer.appendleft(data)
+    
+    def dequeue(self):
+        return self.buffer.pop()
+    
+    def size(self):
+        return len(self.buffer)
+    
+    def is_empty(self):
+        return len(self.buffer) == 0
+    
