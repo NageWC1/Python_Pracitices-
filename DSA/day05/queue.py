d3c60b898cccf8357b2d2194e433ca0c2e1e1516
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-06 09:09:25 +0530

    the functionlity of the queue is done, next is testing

---
+++ DSA/day05/queue.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def enqueue(self, data):
+        self.buffer.appendleft(data)
+    
+    def dequeue(self, data):
+        return  self.buffer.pop()
+    
+    def size(self):
+        return len(self.buffer)
+    
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+if __name__ == "__main__":
+    q = Queue()
+    
