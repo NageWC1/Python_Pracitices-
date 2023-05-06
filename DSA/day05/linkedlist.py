0939565d1b30b63f753b740e9ada01d1d38ac6af
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-06 08:58:24 +0530

    the implementaions of the linkedin functions are done, going to check

--- DSA/day05/linkedlist.py
+++ DSA/day05/linkedlist.py
@@ -7,23 +7,75 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insert_at_beg(self, dat):
-        pass
+    def insert_at_beg(self, data):
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data):
-        pass 
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
 
-    def insert_after_at(self, data):
-        pass 
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
 
-    def insert_before_at(self, data):
-        pass
+        node = Node(data, None)
+        itr.next = node 
 
-    def remove_at(self):
-        pass 
+    def insert_after_at(self, ind, data):
+        count = 0
+        itr = self.head 
+
+        while itr:
+            if count == ind:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next 
+            count += 1
+
+    def insert_before_at(self, ind, data):
+        count = 0
+        itr = self.head 
+
+        while itr:
+            if ind == 0:
+                raise Exception("can take 0 position")
+            if count == ind - 1:
+                node = Node(itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next 
+            count += 1
+
+    def remove_at(self, ind):
+        count = 0
+        itr = self.head
+
+        while itr:
+            if ind == 0:
+                self.head == itr.next
+                break 
+            if count  == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next 
+            count += 1  
 
     def print(self):
-        pass
+        l_str = ""
+        itr = self.head
+        while itr:
+            l_str += str(itr.data) + "-->"
+            itr = itr.next
+        print(l_str)
 
 if __name__ == "__main__":
     l = LinkedList()
+    l.insert_at_beg(10)
+    l.insert_at_beg(11)
+    l.insert_at_beg(12)
+    l.insert_at_beg(13)
+    l.print()
