3c94a9b4cabc7c41a44ae490a059e517f20767e7
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-06 08:45:45 +0530

    the functionf the linked list was define, noww going to implement it

---
+++ DSA/day05/linkedlist.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data 
+        self.next = next 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beg(self, dat):
+        pass
+
+    def insert_at_end(self, data):
+        pass 
+
+    def insert_after_at(self, data):
+        pass 
+
+    def insert_before_at(self, data):
+        pass
+
+    def remove_at(self):
+        pass 
+
+    def print(self):
+        pass
+
+if __name__ == "__main__":
+    l = LinkedList()
