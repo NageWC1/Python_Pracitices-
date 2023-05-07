1b20bfbd9344aef7f1fad71a2fb96f9f2d22a9a5
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-07 20:41:19 +0530

    the all functionality of the linked list were done, today practice of DSA successfully finished

--- DSA/day06/linkedlist.py
+++ DSA/day06/linkedlist.py
@@ -45,6 +45,32 @@ class LinkedList:
             l_str += str(itr.data) + "-->"
             itr = itr.next 
         print(l_str)
+    
+    def insert_after_at(self,ind, data):
+        itr = self.head 
+        count = 0
+
+        while itr:
+            if count == ind:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_before_at(self, ind, data):
+        itr = self.head 
+        count = 0 
+        while itr:    
+            if ind == 0:
+                node = Node(data, self.head)
+                self.head = node
+                break
+            if count == ind -1:
+                node = (data, itr.next)
+                itr.next = node
+                break
+            
 
 if __name__ == "__main__":
     l = LinkedList()
@@ -60,4 +86,8 @@ if __name__ == "__main__":
     l.print()
     l.remove_at(3)
     l.remove_at(0)
+    l.print()
+    l.insert_after_at(0, 19898)
+    l.print()
+    l.insert_before_at(0,9999)
     l.print()
