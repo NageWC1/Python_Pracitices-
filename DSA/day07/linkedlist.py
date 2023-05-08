fd6d75b49f8e6a09b4f412cc3c7bf68a92392cf4
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-08 20:54:07 +0530

    there is a bug in the linked list, have to fix it

--- DSA/day07/linkedlist.py
+++ DSA/day07/linkedlist.py
@@ -45,6 +45,34 @@ class LinkedList:
             l_str += str(itr.data) + "-->"
             itr = itr.next
         print(l_str)
+    
+    def insert_after_at(self, ind, data):
+        itr = self.head 
+        count = 0
+        while itr:
+            if count == ind:
+                node = (data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+    
+    def insert_before_at(self,ind,data):
+        itr = self.head
+        count = 0 
+        
+        while itr:
+            if ind == 0:
+                node = Node(data, self.head)
+                self.head = node
+                break
+            if count == ind - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr =itr.next
+            count += 1
+
 
 if __name__ == "__main__":
     l = LinkedList()
@@ -61,3 +89,9 @@ if __name__ == "__main__":
     l.remove_at(0)
     l.remove_at(5)
     l.print()
+    l.insert_before_at(0,1333)
+    l.insert_before_at(0,1334)
+    l.print()
+    l.insert_after_at(4,7777)
+    l.print()
+    l.insert_after_at(0,1000)
