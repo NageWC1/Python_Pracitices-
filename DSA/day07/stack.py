0d40ccf2176711a3fb1d669461bca6af40ac6220
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-08 20:58:15 +0530

    the stack structure implementation was done

---
+++ DSA/day07/stack.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def insert(self, data):
+        self.buffer.append(data)
+    
+    def pop(self):
+        return self.buffer.pop()
+    
+    def size(self):
+        return len(self.buffer)
+    
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+if __name__ == "__main__":
+    s = Stack()
+    s.insert(10)
+    s.insert(11)
+    s.insert(12)
+    s.insert(13)
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
+    print(s.is_empty())
