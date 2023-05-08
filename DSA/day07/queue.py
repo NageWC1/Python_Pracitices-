fb45c61c643ebc5ef916d46015b0203a098ade18
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-08 20:21:48 +0530

    the basic functionalit of the linkedin implementation was done, next going to be test

--- DSA/day07/queue.py
+++ DSA/day07/queue.py
@@ -8,11 +8,22 @@ class queue:
         self.buffer.appendleft(data)
     
     def dequeue(self):
-        self.buffer.pop()
+        return self.buffer.pop()
 
     def size(self):
         return len(self.buffer)
     
     def is_empty(self):
         return len(self.buffer) == 0
+
+if __name__ == "__main__":
+    q = queue()
+    q.enqueu(10)
+    q.enqueu(11)
+    q.enqueu(12)
+    q.enqueu(13)
+    print(q.dequeue())
+    print(q.dequeue())
+    print(q.size())
+    print(q.is_empty())
     
