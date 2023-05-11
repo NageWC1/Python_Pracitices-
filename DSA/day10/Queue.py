0f3eec9938b1b8dada68f93bf00846f2d47bdf18
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-11 22:20:00 +0530

    the function implementaion was done for the queue and all working perfectly, next stack

--- DSA/day10/Queue.py
+++ DSA/day10/Queue.py
@@ -5,16 +5,16 @@ class Queueu:
         self.buffer = deque()
     
     def enqueue(self, data):
-        pass
+        self.buffer.appendleft(data)
 
-    def dequeue(seflf):
-        pass 
+    def dequeue(self):
+        return self.buffer.pop()
 
     def size(self):
-        pass 
+        return len(self.buffer)
 
     def is_empty(self):
-        pass
+        return len(self.buffer) == 0
 
 if __name__ == "__main__":
     q = Queueu()
