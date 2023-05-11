dfb6b8a280b03f5bf28482c39b162cba518cd340
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-11 21:56:39 +0530

    the linked list functionality were done, going to test

--- DSA/day10/linkelist.py
+++ DSA/day10/linkelist.py
@@ -8,16 +8,40 @@ class LinkedList:
         self.head = None
     
     def insert_at_bg(self, data):
-        pass
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+        itr = self.head 
+        
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
 
     def remove_at(self, ind):
-        pass
+        itr = self.head 
+        count = 0
+        while itr:
+            if ind == 0:
+                self.head = itr.next 
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next 
+                break
+            itr = itr.next 
+            count += 1
 
     def print(self):
-        pass
+        l_str = ""
+        itr = self.head 
+        while itr:
+            l_str += str(itr.data) + "-->"
+            itr = itr.next
+        print(l_str)
 
 if __name__ == "__main__":
     l = LinkedList()
