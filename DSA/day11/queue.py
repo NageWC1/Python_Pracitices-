e78d2f3fe244d4151ff6005d3d7de5a342a55f08
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-12 21:09:37 +0530

    the queue is working finely and conclude the day 11 practice

--- DSA/day11/queue.py
+++ DSA/day11/queue.py
@@ -2,7 +2,7 @@ from collections import deque
 
 class Queue:
     def __init__(self):
-        self.buffer = deque
+        self.buffer = deque()
     
     def enqueu(self, data):
         self.buffer.appendleft(data)
@@ -18,3 +18,13 @@ class Queue:
 
 if __name__ == "__main__":
     q = Queue()
+    q.enqueu(10)
+    q.enqueu(11)
+    q.enqueu(12)
+    q.enqueu(13)
+    print(q.dequeu())
+    print(q.dequeu())
+    print(q.dequeu())
+    print(q.dequeu())
+    print(q.size())
+    print(q.is_empty())
