10d7a1781bcad05a5f0054cb71d74a2395596f2a
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-14 17:53:20 +0530

    the stack functions implementation was done, going to test

---
+++ DSA/day13/stack.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.buffer  = deque
+    
+    def insert(self, data):
+        self.buffer.append(data)
+    
+    def pop(self):
+        return self.buffer.pop()
+    
+    def size(self):
+        return len(self.buffer)
+    
+    def is_empty(self):
+        return (self.buffer) == 0
+
+if __name__ == "main":
+    s = Stack()
