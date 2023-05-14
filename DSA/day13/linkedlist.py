fea28448e3b3b96a15db1a71dd6d4b7baa3deaa5
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-14 18:15:01 +0530

    the linked list functions were done,  conclude the practice of day 13

--- DSA/day13/linkedlist.py
+++ DSA/day13/linkedlist.py
@@ -42,6 +42,35 @@ class LinkedList:
             l_str += str(itr.data) + "-->"
             itr = itr.next 
         print(l_str)
+    
+    def insert_bef_at(self, ind, data):
+        itr = self.head 
+        count  = 0
+        while itr:
+            if ind == 0:
+                node = Node(data, itr.next)
+                self.head = node
+                break
+            if count == ind - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next 
+            count += 1
+    def insert_after_at(self, ind, data):
+        itr = self.head 
+        count = 0
+        while itr:
+            if ind == 0:
+                node = Node(data, self.head)
+                self.head = node
+                break
+            if count == ind:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
 
 if __name__ == "__main__":
     l = LinkedList()
@@ -57,4 +86,10 @@ if __name__ == "__main__":
     l.print()
     l.remove_at(0)
     l.remove_at(3)
+    l.print()
+    l.insert_bef_at(0,1222)
+    l.insert_bef_at(4,1222)
+    l.print()
+    l.insert_after_at(3, 999)
+    l.insert_after_at(4, 9099)
     l.print()
