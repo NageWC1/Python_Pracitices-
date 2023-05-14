74fe264ef03739448927299783d958b8c4c3b3ee
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-14 18:06:30 +0530

    the functions of linked list were working finely, next implement advance functions for linked list

--- DSA/day13/linkedlist.py
+++ DSA/day13/linkedlist.py
@@ -42,3 +42,19 @@ class LinkedList:
             l_str += str(itr.data) + "-->"
             itr = itr.next 
         print(l_str)
+
+if __name__ == "__main__":
+    l = LinkedList()
+    l.insert_at_beg(10)
+    l.insert_at_beg(11)
+    l.insert_at_beg(12)
+    l.insert_at_beg(13)
+    l.print()
+    l.insert_at_end(100)
+    l.insert_at_end(101)
+    l.insert_at_end(102)
+    l.insert_at_end(103)
+    l.print()
+    l.remove_at(0)
+    l.remove_at(3)
+    l.print()
