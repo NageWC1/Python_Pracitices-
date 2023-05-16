18406c6dc18f68870acf4820c67dd6ced3fb3ffe
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-16 21:12:13 +0530

    the linked list functions were implemented, going to test

--- DSA/day14/linkedlist.py
+++ DSA/day14/linkedlist.py
@@ -8,16 +8,45 @@ class LinkedLsit:
         self.head = None
     
     def insert_at_beg(self,data):
-        pass
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        
+        itr.next = Node(data, None)
 
     def remove_at(self, ind):
-        pass
+        count = 0
+        itr = self.head
+
+        while itr:
+            if ind == 0:
+                self.head = itr.next 
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
 
     def insert_after_at(self, ind, data):
-        pass
+        count = 0
+        itr = self.head 
+
+        while itr:
+            if count == ind:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
 
 if __name__ == "__main__":
     pass
