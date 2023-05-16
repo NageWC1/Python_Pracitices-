7bb27bec03613491ce3ac24a05e21763b2c43e46
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-16 21:04:59 +0530

    the basic format for the implementation of linked list was done, and the tree implementation file was added

---
+++ DSA/day14/linkedlist.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next 
+
+class LinkedLsit:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beg(self,data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def remove_at(self, ind):
+        pass
+
+    def insert_after_at(self, ind, data):
+        pass
+
+if __name__ == "__main__":
+    pass
