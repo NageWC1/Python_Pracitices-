f888a7044734f5045cdceaaeb226d0668b66c41e
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-17 21:14:05 +0530

    the linked list working finely, next tree

--- DSA/day15/linkedlist.py
+++ DSA/day15/linkedlist.py
@@ -43,3 +43,22 @@ class LinkedList:
             l_str += str(itr.data) + "-->"
             itr = itr.next
         print(l_str)
+
+if __name__ == "__main__":
+    l = LinkedList()
+    l.insert_at_beg(10)
+    l.insert_at_beg(11)
+    l.insert_at_beg(12)
+    l.insert_at_beg(13)
+    l.insert_at_beg(14)
+    l.print()
+    l.insert_at_end(100)
+    l.insert_at_end(101)
+    l.insert_at_end(102)
+    l.insert_at_end(103)
+    l.insert_at_end(104)
+    l.print()
+    l.remove_at(0)
+    l.remove_at(4)
+    l.remove_at(2)
+    l.print()
