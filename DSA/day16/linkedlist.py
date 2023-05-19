99d8073b0ffb13c2603dd6f31b13d021cac32d77
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-19 22:42:55 +0530

    the linked list was implemented, going to test

---
+++ DSA/day16/linkedlist.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beg(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+    
+    def remove_at(self, ind):
+        count = 0 
+        itr = self.head 
+
+        while itr:
+            if ind == 0:
+                self.head = itr.next
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.head 
+            count += 1
+    
+    def print(self):
+        itr = self.head 
+        l_str = ""
+        while itr:
+            l_str += str(itr.data) + "-->"
+            itr = itr.next 
+        print(l_str)
+
