0cc7a7aa02c375dbc359d86e7ae117b4e3eefd9c
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-19 22:17:10 +0530

    finished the tree data structure

--- DSA/day16/tree.py
+++ DSA/day16/tree.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.parent = None
     
 
-    def addChild(self, child):
+    def add_child(self, child):
         child.parent = self
         self.children.append(child)
     
@@ -22,3 +22,32 @@ class TreeNode:
         space = " " * level * 3
         prefix = space + "|__" if self.parent else ""
         print(prefix + self.data)
+
+        for child in self.children:
+            child.printTree()
+
+def buildTree():
+    root = TreeNode("Electronic")
+    laptop = TreeNode("laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cellphone")
+    cellphone.add_child(TreeNode("iphone"))
+    cellphone.add_child(TreeNode("sumsung"))
+    cellphone.add_child(TreeNode("vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("LG"))
+    tv.add_child(TreeNode("Galxy"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+    return root
+
+if __name__ == "__main__":
+    root = buildTree()
+    root.printTree()
+    
