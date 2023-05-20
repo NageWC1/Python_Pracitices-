394f3cdec306190644e3649fc312923ae2ba7029
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-20 21:51:41 +0530

    the tree implementation is started

---
+++ DSA/day17/tree.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+    
+    def add_child(self,child):
+        child.parent = self
+        self.children.append(child)
+    
+    def print_tree(self):
+        pass
+
+    def getLeve(self):
+        pass
+
+def builtree():
+    root = TreeNode("Electronic")
+    laptop = TreeNode("laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cellphone")
+    cellphone.add_child(TreeNode("iphone"))
+    cellphone.add_child(TreeNode("sumsung"))
+    cellphone.add_child(TreeNode("vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("LG"))
+    tv.add_child(TreeNode("Galxy"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+    return root
+
+if __name__ == "__main__":
+    root = builtree()
+    root.print_tree()
+    
