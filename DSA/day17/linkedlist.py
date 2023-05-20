8b4ccdac61806f0fe99bc16fa0fc534dd9dfd919
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-20 21:28:18 +0530

    the day 17 practice started, linked functions were listed. goign to implement

---
+++ DSA/day17/linkedlist.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next 
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_beg(self, data):
+        pass 
+
+    def insert_end(self, data):
+        pass
+
+    def remove_at(self, ind):
+        pass
+
+    def print(self):
+        pass
+
