25a46416788e7c1fc085e49b3adca17e98aca939
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-20 21:38:10 +0530

    the linked list was working finely

--- DSA/day17/linkedlist.py
+++ DSA/day17/linkedlist.py
@@ -8,14 +8,53 @@ class LinkedList:
         self.head = None
     
     def insert_beg(self, data):
-        pass 
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data,None)
+            self.head = node
+        
+        itr = self.head 
+        while itr.next:
+            itr = itr.next 
+        itr.next = Node(data, None)
 
     def remove_at(self, ind):
-        pass
+        count = 0
+        itr = self.head 
+        while itr:
+            if ind == 0:
+                self.head = itr.next
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next 
+            count += 1
 
     def print(self):
-        pass
+        l_str = ""
+        itr = self.head
+        while itr:
+            l_str += str(itr.data) + "-->"
+            itr = itr.next
+        print(l_str)
 
+if __name__ == "__main__":
+    l = LinkedList()
+    l.insert_beg(10)
+    l.insert_beg(11)
+    l.insert_beg(12)
+    l.insert_beg(13)
+    l.print()
+    l.insert_end(100)
+    l.insert_end(101)
+    l.insert_end(102)
+    l.insert_end(103)
+    l.print()
+    l.remove_at(1)
+    l.remove_at(2)
+    l.remove_at(3)
+    l.print()
