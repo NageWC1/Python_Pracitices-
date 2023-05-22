7a9d252fa797e22182dc5d5edd139a311bb7dfc6
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-22 19:50:01 +0530

    the linked list basic functions were implemented, going to test with main method

--- DSA/day18/linkedlist.py
+++ DSA/day18/linkedlist.py
@@ -8,16 +8,38 @@ class LinkedList:
         self.head = None
     
     def insert_beg(self, data):
-        pass
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_end(self, data):
-        pass
+        if self.head is None:
+            self.head = Node(data, None)
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
 
-    def remove_at(self):
-        pass
+    def remove_at(self, ind):
+        count = 0
+        itr = self.head 
+
+        while itr: 
+            if ind == 0:
+                self.head = itr.next
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next 
+                break
+            itr = itr.next
+
 
     def print(self):
-        pass
+        l_str = ""
+        itr = self.head
+        while itr:
+            l_str += str(itr.data) + "-->"
+            itr = itr.next
+        print(l_str)
 
 if __name__ == "__main__":
     pass
