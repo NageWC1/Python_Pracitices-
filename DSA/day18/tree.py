28cbd07c57fb9b36508c76b5e91f8a13d05165e2
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-22 19:36:58 +0530

    the tree functions were implemented, going to test it

--- DSA/day18/tree.py
+++ DSA/day18/tree.py
@@ -9,7 +9,49 @@ class TreeNode:
         self.children.append(child)
     
     def printTree(self):
-        pass
+        space = self.getLevel() * " " * 3
+        prefix = space + "|__" if self.parent else ""
+        print(self.data)
+        for child in self.children:
+            self.printTree()
 
     def getLevel(self):
-        pass
+        level = 0
+        p = self.parent
+        while p:
+            p = p.parent
+            level += 1
+        return level
+
+
+def buildTree():
+    root = TreeNode("Electronic")
+    laptop = TreeNode("Laptop")
+    tv = TreeNode("TV")
+    phone = TreeNode("Phone")
+
+    assus = TreeNode("assus")
+    xp = TreeNode("xp")
+    acer = TreeNode("acer")
+    laptop.add_child(assus)
+    laptop.add_child(xp)
+    laptop.add_child(acer)
+
+    lg = TreeNode("LG")
+    galaxy = TreeNode("galaxy")
+    lenova = TreeNode("Lenova")
+    tv.add_child(lg)
+    tv.add_child(galaxy)
+    tv.add_child(lenova)
+
+    iphone = TreeNode("Iphone")
+    samsung = TreeNode("Samsung")
+    pad = TreeNode("Pad")
+    phone.add_child(iphone)
+    phone.add_child(samsung)
+    phone.add_child(pad)
+
+    return root
+
+if __name__ == "__main__":
+    pass
