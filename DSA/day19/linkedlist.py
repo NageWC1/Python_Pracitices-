9559fd695521b55b378e9d517ab1a6f182989c57
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-24 20:20:48 +0530

    the liked list functions were defined, next going to implement

---
+++ DSA/day19/linkedlist.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next
+    
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beg(self, data):
+        pass 
+
+    def insert_at_end(self, data):
+        pass
+
+    def remove_at(self, ind):
+        pass
+
+    def print(self):
+        pass
+
+    def insert_before_at(self, ind, data):
+        pass
+
+if __name__ == "__main__":
+    l = LinkedList()
