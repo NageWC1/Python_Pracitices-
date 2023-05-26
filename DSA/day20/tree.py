87d093cd61aaff5526ed9351afa71f52e9d3d5e4
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-26 20:31:21 +0530

    the tree functions are perfectly working,next linkedlist

--- DSA/day20/tree.py
+++ DSA/day20/tree.py
@@ -22,7 +22,39 @@ class TreeNode:
         print(prefix+self.data)
         for child in self.children:
             child.print_tree()
-            
+
+def build_tree():
+    root = TreeNode("Electronic")
+    laptop = TreeNode("Laptop")
+    tv = TreeNode("TV")
+    phone = TreeNode("Phone")
+
+    assus = TreeNode("assus")
+    xp = TreeNode("xp")
+    acer = TreeNode("acer")
+    laptop.add_child(assus)
+    laptop.add_child(xp)
+    laptop.add_child(acer)
+
+    lg = TreeNode("LG")
+    galaxy = TreeNode("galaxy")
+    lenova = TreeNode("Lenova")
+    tv.add_child(lg)
+    tv.add_child(galaxy)
+    tv.add_child(lenova)
+
+    iphone = TreeNode("Iphone")
+    samsung = TreeNode("Samsung")
+    pad = TreeNode("Pad")
+    phone.add_child(iphone)
+    phone.add_child(samsung)
+    phone.add_child(pad)
+
+    root.add_child(laptop)
+    root.add_child(phone)
+    root.add_child(tv)
+    return root
 
 if __name__ == "__main__":
-    pass
+    root = build_tree()
+    root.print_tree()
