8359f6b888566f4e63df1b07a92fb88a00a27ef7
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-26 20:39:40 +0530

    the linekd list functions were implelemented, next goign to test it

--- DSA/day20/linkedlist.py
+++ DSA/day20/linkedlist.py
@@ -8,16 +8,34 @@ class LinkedList:
         self.head = None
     
     def insert_beg(self, data):
-        pass
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
 
     def print(self):
         pass
 
     def remove_at(self, ind):
-        pass
+        count = 0
+        itr = self.head
+        while itr:
+            if ind == 0:
+                self.head = itr.next
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next 
+            count += 1
 
 if __name__ == "__main__":
     l = LinkedList()
+    
