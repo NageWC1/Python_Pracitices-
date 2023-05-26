c3858ea34a0fe543983bfabcd9c7f127005d3322
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-26 20:26:15 +0530

    the day 20 pratice was started, the on going implementatio is tree, goign to finish the tree implementation

---
+++ DSA/day20/tree.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+    
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+    
+    def get_leve(self):
+        pass
+
+if __name__ == "__main__":
+    pass
