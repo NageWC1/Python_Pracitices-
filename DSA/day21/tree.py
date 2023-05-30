c120ccd2b23f0de92331150dbcb40f1036435d7d
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-05-30 21:30:13 +0530

    the functions were impleented, goign to implement build tree method to test

--- DSA/day21/tree.py
+++ DSA/day21/tree.py
@@ -6,4 +6,18 @@ class TreeNode:
     
     def add_child(self, child):
         child.parent = self
-        
+    
+    def getLevel(self):
+        level = 0
+        p = self.parent 
+        while p:
+            p = p.parent
+            level += 1
+        return level
+    
+    def print(self):
+        space  = self.getLevel  * " " * 3
+        prefix = space + "|__" if self.parent else ""
+        print(prefix + self.data)
+        for child in self.children:
+            child.print()
