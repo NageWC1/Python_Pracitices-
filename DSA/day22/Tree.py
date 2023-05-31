2a8ea491b0cf3bf6d0e70a8f4eee6606b6629eb1
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-05-31 14:45:29 +0530

    the tree build method is done, going to test the implementation

--- DSA/day22/Tree.py
+++ DSA/day22/Tree.py
@@ -19,6 +19,43 @@ class TreeNode:
     def print(self):
         space = self.getLevel() * " " * 3
         prefix = space + "|__" if self.parent else ""
+        print(prefix + self.data)
         for child in self.children:
             child.print()
-        
+
+def buildTree():
+    root = TreeNode("Electronic")
+
+    laptop = TreeNode("Laptop")
+    tv = TreeNode("TV")
+    phone = TreeNode("Phone")
+
+    hp = TreeNode("HP")
+    lenova= TreeNode("Lenova")
+    mac = TreeNode("Macbook")
+
+    sony = TreeNode("Sony")
+    singer = TreeNode("Singer")
+    abanse = TreeNode("Abans")
+
+    apple = TreeNode("Apple")
+    oppo = TreeNode("oppo")
+    samsung = TreeNode("Sumsung")
+
+    laptop.add_child(hp)        
+    laptop.add_child(lenova)        
+    laptop.add_child(mac)
+
+    phone.add_child(apple)
+    phone.add_child(oppo)
+    phone.add_child(samsung)
+
+    tv.add_child(sony)        
+    tv.add_child(singer)        
+    tv.add_child(abanse)        
+
+    root.add_child(laptop)
+    root.add_child(tv)
+    root.add_child(phone)
+
+    return root
