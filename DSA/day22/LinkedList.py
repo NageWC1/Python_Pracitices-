68b8f4439e12ba52655447d1092b880e0610db62
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-05-31 15:14:50 +0530

    the functions were implemented, going to test

--- DSA/day22/LinkedList.py
+++ DSA/day22/LinkedList.py
@@ -8,19 +8,53 @@ class LinkedList:
         self.head = None
     
     def insert_at_beg(self, data):
-        pass
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
 
     def insert_before_at(self, ind, data):
-        pass
+        count = 0
+        itr = self.head 
+        while itr:
+            if ind == 0:
+                node = Node(data, self.head)
+                self.head = node
+                break
+            if count == ind - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
 
     def remove_at(self, ind):
-        pass
+        count = 0
+        itr = self.head 
+        while itr:
+            if ind == 0:
+                self.head = itr.next
+                break
+            if count == ind - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
 
     def print(self):
-        pass
+        l_str = ""
+        itr = self.head
+        while itr:
+            l_str += str(itr.data)
+            itr = itr.next
+        print(l_str)
 
 if __name__ == "__main__":
     pass
