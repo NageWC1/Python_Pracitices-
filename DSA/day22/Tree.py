be684b19db554aa9dbaa48509fb5e4e5d4694827
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-05-31 14:29:19 +0530

    the treee functions were implemented and now buid tree method will be implement

--- DSA/day22/Tree.py
+++ DSA/day22/Tree.py
@@ -4,4 +4,21 @@ class TreeNode:
         self.parent = None
         self.children = []
     
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
     
+    def getLevel(self):
+        level = 0
+        p = self.parent
+        while p:
+            p = p.parent 
+            level += 1
+        return level
+
+    def print(self):
+        space = self.getLevel() * " " * 3
+        prefix = space + "|__" if self.parent else ""
+        for child in self.children:
+            child.print()
+        
