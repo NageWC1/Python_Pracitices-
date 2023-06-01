abde8377ef00b6e19c89f6882879452abacf3a50
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-06-01 16:28:15 +0530

    the tree is working finely, the day 23rd practice successfully done

--- DSA/day23/tree.py
+++ DSA/day23/tree.py
@@ -22,7 +22,42 @@ class TreeNode:
         print(prefix + self.data)
         for child in self.children:
             child.print()
+def Buildtree():
+    root = TreeNode("Electrnic")
+    
+    laptop = TreeNode("Laptop")
+    tv = TreeNode("TV")
+    phone = TreeNode("Phone")
 
+    hp = TreeNode("HP")
+    lenova= TreeNode("Lenova")
+    mac = TreeNode("Macbook")
+
+    sony = TreeNode("Sony")
+    singer = TreeNode("Singer")
+    abanse = TreeNode("Abans")
+
+    apple = TreeNode("Apple")
+    oppo = TreeNode("oppo")
+    samsung = TreeNode("Sumsung")
+
+    laptop.add_child(hp)        
+    laptop.add_child(lenova)        
+    laptop.add_child(mac)
+
+    phone.add_child(apple)
+    phone.add_child(oppo)
+    phone.add_child(samsung)
+
+    tv.add_child(sony)        
+    tv.add_child(singer)        
+    tv.add_child(abanse)        
+
+    root.add_child(laptop)
+    root.add_child(tv)
+    root.add_child(phone)
+
+    return root 
 if __name__ == "__main__":
     root = Buildtree()
     root.print()
