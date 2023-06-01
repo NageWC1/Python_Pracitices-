e83ed112df98b88d91875db268a6a75ab8e3d086
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-06-01 16:21:28 +0530

    the tree function were defined, next will be implementation

---
+++ DSA/day23/tree.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+    
+    def add_child(self, child):
+        pass
+
+    def get_level(self):
+        pass
+
+    def print(self):
+        pass
+
+if __name__ == "__main__":
+    pass
