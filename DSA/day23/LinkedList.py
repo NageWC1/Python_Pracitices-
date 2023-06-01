f6446e7f255f8594cc5d4ed7971c69ef1ac9db7d
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-06-01 06:15:17 +0530

    the linked list, functions were defined, next going to implement it

---
+++ DSA/day23/LinkedList.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data 
+        self.next = next 
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_beg(self, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def insert_after_at(self, data):
+        pass 
+
+    def print(self):
+        pass
+
+    def remove_at(self, ind):
+        pass
