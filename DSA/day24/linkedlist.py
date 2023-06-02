e69bab02c01ba54fa2f9611addce52179cb7a20e
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-06-02 09:41:36 +0530

    the linked list is implemented and going to test it

--- DSA/day24/linkedlist.py
+++ DSA/day24/linkedlist.py
@@ -7,14 +7,59 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insert_at_start(slef, data):
-        pass
+    def insert_at_start(self, data):
+        node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data):
-        pass
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+        
+        itr = self.head 
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
 
     def insert_before(self, ind, data):
-        pass
+        count = 0
+        itr = self.head 
+        while itr:
+            if ind == 0:
+                node = Node(data, self.head)
+                self.head = node
+                break 
+            if count == ind - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next 
+            count += 1
 
     def print(self):
-        pass
+        itr = self.head 
+        l_str = ""
+        while itr:
+            l_str += str(self.data) + "-->"
+            itr = itr.next 
+        print(l_str)
+
+    def remove_at(self, ind):
+        count = 0
+        itr =self.head
+        while itr:
+            if ind == 0:
+                self.head  = itr.next
+                break
+            if count == ind - 1:
+                itr.next == itr.next.next 
+                break
+            itr = itr.next
+            count += 1
+
+if __name__ == "__main__":
+    l =LinkedList()
+    l.insert_at_start(10)
+    l.insert_at_start(11)
+    l.insert_at_start(12)
+    l.insert_at_start(13)
