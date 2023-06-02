56c41beb20c5f9e5cff3ed074c589e0ab008f9e2
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-06-02 10:11:07 +0530

    the tree basuc functuions were defined, next goign to implement and test it

---
+++ DSA/day24/tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __inti__(self, data):
+        self.data = data
+        self.childre = []
+        self.parent = None
+    
+    def add_child(self, child):
+        pass
+
+    def getLevel(self):
+        pass
+
+    def print(self):
+        pass
+
+def buildTree():
+    pass
+
+if __name__ == "__main__":
+    pass
