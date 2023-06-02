6780acac0b82d2ee324bc76b256c922684959ceb
Nagenthiran <Nagenthirannagarajh@gmail.com>
2023-06-02 09:33:36 +0530

    the 25th day practice started, now working on linked list

---
+++ DSA/day24/linkedlist.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data:None, next:None):
+        self.data = data
+        self.next = next 
+        self.parent = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_start(slef, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def insert_before(self, ind, data):
+        pass
+
+    def print(self):
+        pass
